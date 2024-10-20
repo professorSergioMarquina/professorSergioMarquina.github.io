3290689a1aa74830fcd13e8013fb0f4713dd71e0
professorSergioMarquina <101969477+professorSergioMarquina@users.noreply.github.com>
2024-10-20 19:23:31 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,9 @@
+document.getElementById('contactForm').addEventListener('submit', function(e) {
+    e.preventDefault();
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const message = document.getElementById('message').value;
+
+    alert(`Thank you, ${name}! Your message has been received.`);
+    // You can add functionality to send this data to a server or email
+});
